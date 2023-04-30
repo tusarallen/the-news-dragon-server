e82a0539f11b0f9f4eee9574bd8ae85cd2ec1439
email <allentusar909@gmail.com>
2023-04-30 09:48:14 +0600

    vercel file added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const catagories = require("./data/catagories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Dragon is running");
@@ -13,6 +14,27 @@ app.get("/", (req, res) => {
 
 app.get("/catagories", (req, res) => {
   res.send(catagories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/catagories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const catagoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(catagoryNews);
+  }
 });
 
 app.listen(port, () => {
